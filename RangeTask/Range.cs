0dc506cc502dffb253aeaf330a0471db79d0dd23
ritak <ritak@LAPTOP-SAGA1KBL>
2024-07-18 11:38:18 +0700

    Range, RangeCalculation

---
+++ RangeTask/Range.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace RangeTask
+{
+    class Range
+    {
+        public double From { get; set; }
+
+        public double To { get; set; }
+
+        public Range(double from, double to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public double GetLength()
+        {
+            return To - From;
+        }
+
+        public bool IsInside(double number)
+        {
+            return number >= From && number <= To;
+        }
+
+        public Range Intersect(Range otherRange)
+        {
+            if (otherRange.From >= To || otherRange.To <= From)
+            {
+                return null;
+            }
+
+            return new Range(Math.Max(From, otherRange.From), Math.Min(To, otherRange.To));
+        }
+
+        public Range[] Union(Range otherRange)
+        {
+            if (otherRange.From > To || otherRange.To < From)
+            {
+                return new Range[] { new Range(From, To), new Range(otherRange.From, otherRange.To) };
+            }
+
+            return new Range[] { new Range(Math.Min(From, otherRange.From), Math.Max(To, otherRange.To)) };
+        }
+
+        public Range[] Difference(Range otherRange)
+        {
+            if (otherRange.From <= From && otherRange.To >= To)
+            {
+                return null;
+            }
+            if (otherRange.From > From && otherRange.To > To)
+            {
+                return new Range[] { new Range(From, otherRange.From) };
+            }
+            else if (otherRange.From < From && otherRange.To < To)
+            {
+                return new Range[] { new Range(otherRange.To, To) };
+            }
+
+            return new Range[] { new Range(From, otherRange.From), new Range(otherRange.To, To) };
+        }
+    }
+}
---
+++ RangeTask/RangeCalculation.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RangeTask
+{
+    internal class RangeCalculation
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите начальное число первого диапазона:");
+            double firstRangeStartNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите конечное число первого диапазона:");
+            double firstRangeEndNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите начальное число второго диапазона:");
+            double secondRangeStartNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите конечное число второго диапазона:");
+            double secondRangeEndNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите число:");
+            double number = Convert.ToDouble(Console.ReadLine());
+
+            Range firstRange = new Range(firstRangeStartNumber, firstRangeEndNumber);
+            Range secondRange = new Range(secondRangeStartNumber, secondRangeEndNumber);
+
+            Console.WriteLine("Длина первого диапазона: " + firstRange.GetLength());
+
+            if (firstRange.IsInside(number))
+            {
+                Console.WriteLine("Введённое число принадлежит первому диапазону");
+            }
+            else
+            {
+                Console.WriteLine("Введённое число не принадлежит диапазону");
+            }
+
+            Range intersectionRange = firstRange.Intersect(secondRange);
+
+            if (intersectionRange != null)
+            {
+                Console.WriteLine("Диапазон пересечения: " + intersectionRange.From + " - " + intersectionRange.To);
+            }
+            else
+            {
+                Console.WriteLine("Нет пересечения");
+            }
+
+            Range[] unionRanges = firstRange.Union(secondRange);
+
+            Console.WriteLine("Объединение диапазонов: ");
+
+            foreach (Range range in unionRanges)
+            {
+                Console.WriteLine(range.From + " - " + range.To);
+            }
+
+            Range[] rangesDifference = firstRange.Difference(secondRange);
+
+            Console.WriteLine("Разность диапазонов: ");
+
+            if (rangesDifference != null)
+            {
+                foreach (Range range in rangesDifference)
+                {
+                    Console.WriteLine(range.From + " - " + range.To);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Нет разности");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
