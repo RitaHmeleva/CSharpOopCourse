09a911c9fa3bda0f48be9360a5be7b8a59263d26
ritak <ritak@LAPTOP-SAGA1KBL>
2024-08-02 15:50:48 +0700

    Vector

---
+++ VectorTask/Vector.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorTask
+{
+    class Vector
+    {
+        private int size;
+        private int[] vector;
+
+        public Vector(int n)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentException("Size n should be > 0");
+            }
+
+            vector = new int[n];
+            size = n;
+
+            for (int i = 0; i < n; i++)
+            {
+                vector[i] = 0;
+            }
+        }
+
+        public Vector(Vector array)
+        {
+            vector = array.vector;
+
+            size = array.size;
+        }
+
+        public Vector(int[] array)
+        {
+            vector = new int[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                vector[i] = array[i];
+            }
+
+            size = array.Length;
+        }
+
+        public Vector(int n, int[] array)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentException("Size n should be > 0");
+            }
+
+            vector = new int[n];
+
+            if (array.Length < n)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    vector[i] = array[i];
+                }
+
+                for (int i = array.Length; i < n; i++)
+                {
+                    vector[i] = 0;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    vector[i] = array[i];
+                }
+            }
+
+            size = n;
+        }
+
+        public int GetSize()
+        {
+            return size;
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", vector);
+        }
+
+        public Vector GetSum(Vector vector2)
+        {
+            int[] vectorSum = new int[Math.Max(size, vector2.size)];
+
+            if (size >= vector2.size)
+            {
+                Vector vec = new Vector(size, vector2.vector);
+
+                for (int i = 0; i < size; i++)
+                {
+                    vectorSum[i] = vector[i] + vec.vector[i];
+                }
+            }
+
+            if (size < vector2.size)
+            {
+                Vector vec = new Vector(vector2.size, vector);
+
+                for (int i = 0; i < vector2.size; i++)
+                {
+                    vectorSum[i] = vec.vector[i] + vector2.vector[i];
+                }
+            }
+
+            return new Vector(vectorSum);
+        }
+
+        public Vector GetDifference(Vector vector2)
+        {
+            int[] vectorDifference = new int[Math.Max(size, vector2.size)];
+
+            if (size >= vector2.size)
+            {
+                Vector vec = new Vector(size, vector2.vector);
+
+                for (int i = 0; i < size; i++)
+                {
+                    vectorDifference[i] = vector[i] + vec.vector[i];
+                }
+            }
+
+            if (size < vector2.size)
+            {
+                Vector vec = new Vector(vector2.size, vector);
+
+                for (int i = 0; i < vector2.size; i++)
+                {
+                    vectorDifference[i] = vec.vector[i] + vector2.vector[i];
+                }
+            }
+
+            return new Vector(vectorDifference);
+        }
+
+        public Vector GetProduct(Vector vector2)
+        {
+            int[] vectorProduct = new int[Math.Max(size, vector2.size)];
+
+            if (size >= vector2.size)
+            {
+                Vector vec = new Vector(size, vector2.vector);
+
+                for (int i = 0; i < size; i++)
+                {
+                    vectorProduct[i] = vector[i] + vec.vector[i];
+                }
+            }
+
+            if (size < vector2.size)
+            {
+                Vector vec = new Vector(vector2.size, vector);
+
+                for (int i = 0; i < vector2.size; i++)
+                {
+                    vectorProduct[i] = vec.vector[i] + vector2.vector[i];
+                }
+            }
+
+            return new Vector(vectorProduct);
+        }
+
+        public Vector GetRiversal()
+        {
+            int[] vectorRiversal = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                vectorRiversal[i] = this.vector[i] * -1;
+            }
+
+            return new Vector(vectorRiversal);
+        }
+        public double GetLength()
+        {
+            double squaresSum = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                squaresSum += Math.Pow(vector[i], 2);
+            }
+
+            return Math.Sqrt(squaresSum);
+        }
+
+        public int? GetComponent(int index)
+        {
+            if (index < size)
+            {
+                return vector[index];
+            }
+
+            return null;
+        }
+
+        public Vector SetComponent(int index, int component)
+        {
+            if (index < size)
+            {
+                int[] vectorRiversal = vector;
+
+                vectorRiversal[index] = component;
+
+                return new Vector(vectorRiversal);
+            }
+
+            return new Vector(vector);
+        }
+
+        public override int GetHashCode()
+        {
+            int prime = 37;
+            int hash = 1;
+
+            for (int i = 0; i < size; i++)
+            {
+                hash = prime * hash + vector[i];
+            }
+
+            return hash;
+        }
+
+        public override bool Equals(object o)
+        {
+            if (ReferenceEquals(o, this))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(o, null) || o.GetType() != GetType())
+            {
+                return false;
+            }
+
+            Vector v = (Vector)o;
+
+            if (v.size == size)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    if (v.vector[i] != vector[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return vector == v.vector;
+        }
+
+        public static Vector GetSum(Vector vector1, Vector vector2)
+        {
+            int[] vectorSum = new int[Math.Max(vector1.size, vector2.size)];
+
+            if (vector1.size >= vector2.size)
+            {
+                Vector vec = new Vector(vector1.size, vector2.vector);
+
+                for (int i = 0; i < vector1.size; i++)
+                {
+                    vectorSum[i] = vector1.vector[i] + vec.vector[i];
+                }
+            }
+
+            if (vector1.size < vector2.size)
+            {
+                Vector vec = new Vector(vector2.size, vector1.vector);
+
+                for (int i = 0; i < vector2.size; i++)
+                {
+                    vectorSum[i] = vec.vector[i] + vector2.vector[i];
+                }
+            }
+
+            return new Vector(vectorSum);
+        }
+
+        public static Vector GetDifference(Vector vector1, Vector vector2)
+        {
+            int[] vectorDifference = new int[Math.Max(vector1.size, vector2.size)];
+
+            if (vector1.size >= vector2.size)
+            {
+                Vector vec = new Vector(vector1.size, vector2.vector);
+
+                for (int i = 0; i < vector1.size; i++)
+                {
+                    vectorDifference[i] = vector1.vector[i] + vec.vector[i];
+                }
+            }
+
+            if (vector1.size < vector2.size)
+            {
+                Vector vec = new Vector(vector2.size, vector1.vector);
+
+                for (int i = 0; i < vector2.size; i++)
+                {
+                    vectorDifference[i] = vec.vector[i] + vector2.vector[i];
+                }
+            }
+
+            return new Vector(vectorDifference);
+        }
+
+        public static Vector GetProduct(Vector vector1, Vector vector2)
+        {
+            int[] vectorProduct = new int[Math.Max(vector1.size, vector2.size)];
+
+            if (vector1.size >= vector2.size)
+            {
+                Vector vec = new Vector(vector1.size, vector2.vector);
+
+                for (int i = 0; i < vector1.size; i++)
+                {
+                    vectorProduct[i] = vector1.vector[i] + vec.vector[i];
+                }
+            }
+
+            if (vector1.size < vector2.size)
+            {
+                Vector vec = new Vector(vector2.size, vector1.vector);
+
+                for (int i = 0; i < vector2.size; i++)
+                {
+                    vectorProduct[i] = vec.vector[i] + vector2.vector[i];
+                }
+            }
+
+            return new Vector(vectorProduct);
+        }
+    }
+}
---
+++ VectorTask/VectorTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorTask
+{
+    internal class VectorTest
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            int[] array = { 1, 4, 8 };
+            int[] array2 = { 7, 4 };
+
+            Vector vector = new Vector(n);
+            Vector vector3 = new Vector(array);
+            Vector vector4 = new Vector(n, array2);
+            Vector vector2 = new Vector(vector3);
+
+            Console.WriteLine(vector + "   Размер:" + vector.GetSize());
+            Console.WriteLine(vector3 + "   Размер:" + vector3.GetSize());
+            Console.WriteLine(vector4 + "   Размер:" + vector4.GetSize());
+            Console.WriteLine(vector2 + "   Размер:" + vector2.GetSize());
+
+            Console.WriteLine("Прибавление: " + vector2.GetSum(vector4));
+            Console.WriteLine("Вычитание: " + vector2.GetDifference(vector4));
+            Console.WriteLine("Произведение: " + vector2.GetProduct(vector4));
+            Console.WriteLine("Разворот: " + vector2.GetRiversal());
+            Console.WriteLine("Длина: " + vector2.GetLength());
+            Console.WriteLine("Компонента по индексу: " + vector2.GetComponent(2));
+            Console.WriteLine("Компонента по индексу: " + vector2.SetComponent(1, 1));
+
+            Console.WriteLine("Прибавление: " + Vector.GetSum(vector3, vector4));
+            Console.WriteLine("Вычитание: " + Vector.GetDifference(vector3, vector4));
+            Console.WriteLine("Произведение: " + Vector.GetProduct(vector3, vector4));
+
+            Console.WriteLine("Равенство: " + Equals(vector3, vector4));
+
+            Console.ReadLine();
+        }
+    }
+}
