99dbb13372994117a257f5fb25fbbafece292388
ritak <ritak@LAPTOP-SAGA1KBL>
2024-09-10 22:19:49 +0700

    List

---
+++ ListTask/List.cs
@@ -0,0 +1,303 @@
+﻿using System;
+
+namespace ListTask
+{
+    class ListItem<T>
+    {
+        public T Data { get; set; }
+
+        public ListItem<T> Next { get; set; }
+
+        public ListItem(T data)
+        {
+            Data = data;
+        }
+
+        public ListItem(T data, ListItem<T> next)
+        {
+            Data = data;
+            Next = next;
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+
+    class SinglyLinkedList<T>
+    {
+        private ListItem<T> head;
+
+        private int count;
+
+        public SinglyLinkedList()
+        {
+            count = 0;
+        }
+
+        public override string ToString()
+        {
+            string s = null;
+
+            for (ListItem<T> currentItem = head; currentItem != null; currentItem = currentItem.Next)
+            {
+                s += currentItem.Data + " ";
+            }
+
+            return s;
+        }
+
+        public int GetCount()
+        {
+            return count;
+        }
+
+        public ListItem<T> GetFirstElement()
+        {
+            return head;
+        }
+
+        private ListItem<T> GetItem(int index)
+        {
+            ListItem<T> currentItem = head;
+
+            for (int i = 0; i < index; i++)
+            {
+                currentItem = currentItem.Next;
+            }
+
+            return currentItem;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0)
+                {
+                    throw new ArgumentOutOfRangeException("");
+                }
+
+                if (index >= count)
+                {
+                    throw new ArgumentOutOfRangeException("");
+                }
+
+                ListItem<T> currentItem = GetItem(index);
+
+                return currentItem.Data;
+            }
+
+            set
+            {
+                if (index < 0)
+                {
+                    throw new ArgumentOutOfRangeException("");
+                }
+
+                if (index >= count)
+                {
+                    throw new ArgumentOutOfRangeException("");
+                }
+
+                ListItem<T> currentItem = GetItem(index);
+
+                currentItem.Data = value;
+            }
+        }
+
+        public T GetData(int index)
+        {
+            if (index < 0)
+            {
+                throw new ArgumentOutOfRangeException("");
+            }
+
+            if (index >= count)
+            {
+                throw new ArgumentOutOfRangeException("");
+            }
+
+            ListItem<T> currentItem = GetItem(index);
+
+            return currentItem.Data;
+        }
+
+        public T SetData(int index, T data)
+        {
+            if (index < 0)
+            {
+                throw new ArgumentOutOfRangeException("");
+            }
+
+            if (index >= count)
+            {
+                throw new ArgumentOutOfRangeException("");
+            }
+
+            ListItem<T> currentItem = GetItem(index);
+
+            T oldValue = currentItem.Data;
+            currentItem.Data = data;
+
+            return oldValue;
+        }
+
+        public T DeleteElement(int index)
+        {
+            if (index < 0)
+            {
+                throw new ArgumentOutOfRangeException("");
+            }
+
+            if (index >= count)
+            {
+                throw new ArgumentOutOfRangeException("");
+            }
+
+            count--;
+
+            ListItem<T> currentItem = head;
+            ListItem<T> previousItem = head;
+
+            T deletedValue = currentItem.Data;
+
+            if (index == 0)
+            {
+                head = currentItem.Next;
+
+                return deletedValue;
+            }
+
+            for (int i = 0; i < index; i++)
+            {
+                previousItem = currentItem;
+                currentItem = currentItem.Next;
+            }
+
+            deletedValue = previousItem.Next.Data;
+            previousItem.Next = currentItem.Next;
+
+            return deletedValue;
+        }
+
+        public void AddFirst(T element)
+        {
+            head = new ListItem<T>(element, head);
+
+            count++;
+        }
+
+        public void AddByIndex(int index, T element)
+        {
+            if (index < 0)
+            {
+                throw new ArgumentOutOfRangeException("");
+            }
+
+            if (index >= count)
+            {
+                throw new ArgumentOutOfRangeException("");
+            }
+
+            count++;
+
+            ListItem<T> currentItem = head;
+            ListItem<T> previousItem = head;
+            ListItem<T> temp = new ListItem<T>(element);
+
+            if (index == 0)
+            {
+                head = temp;
+            }
+            else
+            {
+                for (int i = 0; i < index; i++)
+                {
+                    previousItem = currentItem;
+                    currentItem = currentItem.Next;
+                }
+
+                previousItem.Next = temp;
+            }
+
+            temp.Next = currentItem;
+        }
+
+        public bool DeleteData(T data)
+        {
+            int changesCount = 0;
+
+            for (ListItem<T> currentItem = head, previousItem = null; currentItem != null; previousItem = currentItem, currentItem = currentItem.Next)
+            {
+                if (currentItem.Data.Equals(data))
+                {
+                    previousItem.Next = currentItem.Next;
+                    changesCount++;
+                    count--;
+                }
+            }
+
+            if (changesCount > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public ListItem<T> GetDeletedFirstElement()
+        {
+            ListItem<T> deletedElement = head;
+
+            head = head.Next;
+
+            count--;
+
+            return deletedElement;
+        }
+
+        public void Reverse()
+        {
+            ListItem<T> currentItem = head;
+
+            for (int i = 1; i <= count/2; i++)
+            {
+                ListItem<T> lastItem = GetItem(count - i);
+                T item = currentItem.Data;
+                currentItem.Data = lastItem.Data;
+                lastItem.Data = item;
+                currentItem = currentItem.Next;
+            }
+        }
+
+        public SinglyLinkedList<T> GetCopy()
+        {
+            SinglyLinkedList<T> newList = new SinglyLinkedList<T>();
+
+            ListItem<T> item = head;
+            ListItem<T> newPrevItem = null;
+
+            while (item != null)
+            {
+                ListItem<T> newItem = new ListItem<T>(item.Data, null);
+
+                if(newPrevItem != null)
+                {
+                    newPrevItem.Next = newItem;
+                }
+                else
+                {
+                    newList.head = newItem;
+                }
+
+                newPrevItem = newItem;
+
+                item = item.Next;
+            }
+
+            return newList;
+        }
+    }
+}
---
+++ ListTask/ListMain.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ListTask
+{
+    internal class ListMain
+    {
+        static void Main(string[] args)
+        {
+            SinglyLinkedList<int> list1 = new SinglyLinkedList<int>();
+
+            list1.AddFirst(8);
+            list1.AddFirst(7);
+            list1.AddFirst(1);
+            list1.AddFirst(2);
+            list1.AddFirst(3);
+            list1.AddFirst(4);
+
+            SinglyLinkedList<int> list2 = new SinglyLinkedList<int>();
+
+            list2.AddFirst(8);
+
+            SinglyLinkedList<string> list3 = new SinglyLinkedList<string>();
+
+            list3.AddFirst("string1");
+            list3.AddFirst("string2");
+            list3.AddFirst("string3");
+
+            Console.WriteLine("Размер списка: " + list1.GetCount());
+
+            Console.WriteLine("Значение первого элемента: " + list1.GetFirstElement());
+
+            Console.WriteLine("Значение элемента по индексу: " + list3.GetData(1));
+
+            Console.WriteLine("Старое значение элемента по индексу: " + list1.SetData(2, 6));
+
+            Console.WriteLine(list1);
+
+            Console.WriteLine("Значение удаленного элемента по индексу: " + list1.DeleteElement(4));
+
+            Console.WriteLine(list1);
+
+            list1.AddFirst(0);
+            Console.WriteLine("Вставка элемента в начало: " + list1);
+
+            list1.AddByIndex(1, 10);
+            Console.WriteLine("Вставка элемента по индексу: " + list1);
+
+            Console.WriteLine("Удаление узла по значению: " + list1.DeleteData(4));
+            Console.WriteLine(list1);
+           
+            Console.WriteLine("Значение удаленного первого элемента: " + list1.GetDeletedFirstElement());
+
+            list1.Reverse();
+            Console.WriteLine("Разворот списка: " + list1);
+
+            Console.WriteLine("Копия списка: " + list2.GetCopy());
+
+            Console.ReadLine();
+        }
+    }
+}
