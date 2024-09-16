7ee68d28608316684155542dd9ce534a3c06ef08
ritak <ritak@LAPTOP-SAGA1KBL>
2024-09-16 23:31:22 +0700

    Tree

---
+++ TreeTask/TreeMain.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeTask;
+
+internal class TreeMain
+{
+    static void Main(string[] args)
+    {
+        BinaryTree<int> tree = new BinaryTree<int>();
+
+        tree.InsertRoot(12);
+        tree.InsertNode(3);
+        tree.InsertNode(14);
+        tree.InsertNode(1);
+        tree.InsertNode(5);
+        tree.InsertNode(13);
+        tree.InsertNode(16);
+        tree.InsertNode(4);
+        tree.InsertNode(9);
+        tree.InsertNode(15);
+        tree.InsertNode(7);
+        tree.InsertNode(10);
+        tree.InsertNode(6);
+        tree.InsertNode(8);
+        tree.InsertNode(11);
+
+        Console.WriteLine(tree.ToString());
+
+        tree.DeleteNode(5);
+        Console.WriteLine("Дерево: ");
+        Console.WriteLine(tree.ToString());
+
+        Console.WriteLine("Обход в глубину с рекурсией: ");
+        tree.ReversalDepthTraversal(tree.root);
+
+        Console.WriteLine("Обход в ширину: ");
+        tree.WidthTraversal();
+
+        Console.WriteLine("Обход в глубину: ");
+        tree.DepthTraversal();
+
+        Console.WriteLine("Число элементов: " + tree.GetCount());
+
+        Console.ReadLine();
+    }
+}
