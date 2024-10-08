27eb4ee6699c00075892bc6c4bef4bc490681525
ritak <ritak@LAPTOP-SAGA1KBL>
2024-10-08 22:25:37 +0700

    Lambdas

--- LambdasTask/Lambdas.cs
+++ LambdasTask/Lambdas.cs
@@ -6,12 +6,12 @@ internal class Lambdas
     {
         var persons = new List<Person>
         {
-            new Person ("Иван", 13),
-            new Person ("Мария", 20),
-            new Person ("Владимир", 33),
-            new Person ("Иван", 40),
-            new Person ("Семён", 28),
-            new Person ("Анна", 15)
+            new Person("Иван", 13),
+            new Person("Мария", 20),
+            new Person("Владимир", 33),
+            new Person("Иван", 40),
+            new Person("Семён", 28),
+            new Person("Анна", 15)
         };
 
         var uniqueNames = persons
@@ -19,39 +19,34 @@ internal class Lambdas
             .Distinct()
             .ToList();
 
-        Console.WriteLine("Имена: " + string.Join(", ", uniqueNames));
+        Console.WriteLine("Имена: " + string.Join(", ", uniqueNames) + ".");
 
         var personsUnderEighteen = persons
-            .Where(p => p.Age <= 18)
+            .Where(p => p.Age < 18)
             .Select(p => p.Name)
             .ToList();
 
-        var averageAge = persons
-            .Where(p => p.Age <= 18)
-            .Select(p => p.Age)
+        double? averageAgeUnderEighteen = persons
+            .Where(p => p.Age < 18)
+            .Select(p => p?.Age)
             .Average();
 
-        Console.WriteLine("Имена людей младше 18: " + string.Join(", ", personsUnderEighteen) + ". Средний возраст: " + averageAge);
+        Console.WriteLine("Имена людей младше 18: " + string.Join(", ", personsUnderEighteen) + ". Средний возраст: " + averageAgeUnderEighteen);
 
-        var namesAverageAges = persons
+        var averageAgesByName = persons
             .GroupBy(p => p.Name)
-            .Select(p => new
-            {
-                Name = p.Key,
-                AverageAge = p.Select(p => p.Age).Average()
-            })
-            .ToDictionary(p => p.Name, p => p.AverageAge)
+            .ToDictionary(n => n.Key, a => a.Average(a => a.Age))
             .ToList();
 
-        Console.WriteLine("Имена со средним возрастом: " + string.Join(", ", namesAverageAges));
+        Console.WriteLine("Имена со средним возрастом: " + string.Join(", ", averageAgesByName));
 
-        var descendingAge = persons
+        var agesBetweenTwentyAndFortyFive = persons
             .Where(p => p.Age >= 20 && p.Age <= 45)
             .OrderByDescending(p => p.Age)
             .Select(p => p.Name)
             .ToList();
 
-        Console.WriteLine("Имена людей, возраст которых от 20 до 45: " + string.Join(", ", descendingAge));
+        Console.WriteLine("Имена людей, возраст которых от 20 до 45: " + string.Join(", ", agesBetweenTwentyAndFortyFive));
 
         Console.ReadLine();
     }
