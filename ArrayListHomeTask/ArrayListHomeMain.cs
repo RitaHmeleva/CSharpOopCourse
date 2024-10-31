b24aeb0902c624fd14bd476119300d73903fce45
ritak <ritak@LAPTOP-SAGA1KBL>
2024-10-31 20:24:59 +0700

    ArrayListHome

--- ArrayListHomeTask/ArrayListHomeMain.cs
+++ ArrayListHomeTask/ArrayListHomeMain.cs
@@ -1,75 +1,78 @@
-﻿using System.Text;
-
-namespace ArrayListHome;
+﻿namespace ArrayListHome;
 
 internal class ArrayListHomeMain
 {
+    public static List<string> ReadFileToList(string fileName)
+    {
+        List<string> stringsList = new List<string>();
+
+        try
+        {
+            using StreamReader reader = new StreamReader(fileName);
+
+            string? currentLine;
+
+            while ((currentLine = reader.ReadLine()) != null)
+            {
+                stringsList.Add(currentLine);
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        return stringsList;
+    }
+
+    public static void RemoveEvenNumbers(List<int> list)
+    {
+        for (int i = list.Count - 1; i >= 0; i--)
+        {
+            if (list[i] % 2 == 0)
+            {
+                list.RemoveAt(i);
+            }
+        }
+    }
+
+    public static  List<T> RemoveRepeatedElements<T>(List<T> list)
+    {
+        List<T> newList = new List<T>(list.Count);
+
+        if (list.Count == 0)
+        {
+            return newList;
+        }
+
+        newList.Add(list[0]);
+
+        for (int i = 1; i < list.Count; i++)
+        {
+            if (!newList.Contains(list[i]))
+            {
+                newList.Add(list[i]);
+            }
+        }
+
+        return newList;
+    }
+
     static void Main(string[] args)
     {
-        List<string> list = new List<string>();
+        List<string> stringsList = ReadFileToList("..\\..\\list.txt");
 
-        void ReadFileToList(string fileName, List<string> list)
+        foreach (string line in stringsList)
         {
-            try
-            {
-                using StreamReader reader = new StreamReader(fileName);
-
-                string? currentLine;
-
-                while ((currentLine = reader.ReadLine()) != null)
-                {
-                    list.Add(currentLine);
-                }
-            }
-            catch (FileNotFoundException fileNotFoundExeption)
-            {
-                Console.WriteLine(fileNotFoundExeption.Message);
-            }
-            catch (IOException ioExeption)
-            {
-                Console.WriteLine(ioExeption.Message);
-            }
-            catch (Exception exeption)
-            {
-                Console.WriteLine(exeption.Message);
-            }
-        }
-
-        void RemoveEvenNumbers(List<int> list)
-        {
-            for (int i = 0; i < list.Count; i++)
-            {
-                if (list[i] % 2 == 0)
-                {
-                    list.RemoveAt(i);
-                    i--;
-                }
-            }
-        }
-
-        void RemoveRepeated(List<int> list)
-        {
-            List<int> newList = new List<int>();
-
-            newList.Add(list[0]);
-
-            for (int i = 1; i < list.Count; i++)
-            {
-                if (!newList.Contains(list[i]))
-                {
-                    newList.Add(list[i]);
-                }
-            }
-
-            list.Clear();
-            list.AddRange(newList);
-        }
-
-        ReadFileToList("..\\..\\list.txt", list);
-
-        foreach (string item in list)
-        {
-            Console.WriteLine(item);
+            Console.WriteLine(line);
         }
 
         Console.WriteLine();
@@ -78,18 +81,18 @@ internal class ArrayListHomeMain
 
         RemoveEvenNumbers(numbersList);
 
-        foreach (int item in numbersList)
+        foreach (int number in numbersList)
         {
-            Console.WriteLine(item);
+            Console.WriteLine(number);
         }
 
         Console.WriteLine();
 
-        RemoveRepeated(numbersList);
+        List<int> distinctList = RemoveRepeatedElements<int>(numbersList);
 
-        foreach (int item in numbersList)
+        foreach (int element in distinctList)
         {
-            Console.WriteLine(item);
+            Console.WriteLine(element);
         }
 
         Console.ReadLine();
